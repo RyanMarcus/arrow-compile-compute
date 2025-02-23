276ff13f689943c40ce3ca9d6dbce34752716c1e
Ryan Marcus <ryan@ryanmarc.us>
2025-02-23 14:48:36 -0500

    Removed unused import

--- benches/filter_bench.rs
+++ benches/filter_bench.rs
@@ -1,4 +1,4 @@
-use arrow_array::{cast::AsArray, types::Int32Type, Array, BooleanArray, Int32Array, StringArray};
+use arrow_array::{cast::AsArray, Array, BooleanArray, Int32Array, StringArray};
 use arrow_compile_compute::dictionary_data_type;
 use arrow_schema::DataType;
 use criterion::{criterion_group, criterion_main, Criterion};
