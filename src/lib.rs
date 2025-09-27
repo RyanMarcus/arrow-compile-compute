f3abba44efb8787a45a037d34336fa6245cf5611
Ryan Marcus <ryan@ryanmarc.us>
2025-09-27 00:29:56 -0400

    Make each global thread-local for concurrent calls

--- src/lib.rs
+++ src/lib.rs
@@ -49,6 +49,7 @@ macro_rules! declare_global_pointer {
     ($module:expr, $label:ident) => {{
         let ptr_type = $module.get_context().ptr_type(AddressSpace::default());
         let global_var = $module.add_global(ptr_type, None, stringify!($label));
+        global_var.set_thread_local(true);
         global_var.set_initializer(&ptr_type.const_null());
         global_var.set_linkage(inkwell::module::Linkage::Private);
         global_var
